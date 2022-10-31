8fc51baf83ac2575ce63f1cff7875fdf59978c5f
BarcosMezquida <laramezquida@gmail.com>
2022-10-31 17:51:02 -0300

    Terminado los condicionales, ciclos, funciones para calculadora funcional

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', () => { /* Para escuchar cuando se carga el documento */
+window.addEventListener('load', () => {
     const display = document.querySelector('.calculator-display')
     const keypadButtons = document.getElementsByClassName('keypad-button');
 
@@ -8,9 +8,42 @@ window.addEventListener('load', () => { /* Para escuchar cuando se carga el docu
 
     keypadButtonsArray.forEach((button) => {
         button.addEventListener('click', () => {
-            console.log(button.innerHTML);
+            calculator(button, display);
         })
 
     })
 });
 
+function calculator(button, display) {
+    switch (button.innerHTML) {
+        case 'C':
+            borrar(display);
+            break;
+
+        case '=':
+            calcular(display);
+            break;
+
+        default:
+            actualizar(display, button);
+            break;
+    }
+
+}
+
+function calcular(display) {
+    display.innerHTML = eval(display.innerHTML)
+
+}
+
+function actualizar(display, button) {
+    if (display.innerHTML == 0) {
+        display.innerHTML = ''
+    }
+    display.innerHTML += button.innerHTML;
+}
+
+function borrar(display) {
+    display.innerHTML = 0;
+
+}
